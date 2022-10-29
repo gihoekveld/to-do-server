9eb81f663193d6910501a153e4fed11c91caa6a6
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-10-28 22:10:34 -0300

    create array tasks to set initial state of tasks

---
+++ src/modules/tasks/repositories/implementations/tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from "../../model/Task";
+
+export const tasks: Task[] = [
+  {
+    "id": "bcb89cc4-6130-49d0-b39d-f1fd86db1779",
+    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    "created_at": "2022-08-01T00:00:00.000Z",
+    "status": "to-do"
+  },
+  {
+    "id": "764c7b98-c7c5-4362-a855-d79277bf71c2",
+    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    "created_at": "2022-09-10T00:00:00.000Z",
+    "status": "to-do"
+  },
+  {
+    "id": "28294ce0-0955-4550-806b-a00791abcdc2",
+    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    "created_at": "2022-10-10T00:00:00.000Z",
+    "status": "to-do"
+  },
+  {
+    "id": "10538f4a-722c-44a8-bbdf-bcd595e0c139",
+    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    "created_at": "2022-10-11T00:00:00.000Z",
+    "status": "done"
+  },
+  {
+    "id": "5718ab6f-8566-474e-980b-5d536bd534d5",
+    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    "created_at": "2022-10-13T00:00:00.000Z",
+    "status": "done"
+  }
+]
