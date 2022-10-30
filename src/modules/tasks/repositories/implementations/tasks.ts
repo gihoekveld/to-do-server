3d5f400a8a84184be39d3710344ff425b3d96b95
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-10-29 21:47:23 -0300

    Update tasks default value

--- src/modules/tasks/repositories/implementations/tasks.ts
+++ src/modules/tasks/repositories/implementations/tasks.ts
@@ -2,33 +2,33 @@ import { Task } from "../../model/Task";
 
 export const tasks: Task[] = [
   {
-    "id": "bcb89cc4-6130-49d0-b39d-f1fd86db1779",
-    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    "created_at": "2022-08-01T00:00:00.000Z",
-    "status": "to-do"
-  },
-  {
-    "id": "764c7b98-c7c5-4362-a855-d79277bf71c2",
-    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    "created_at": "2022-09-10T00:00:00.000Z",
-    "status": "to-do"
-  },
-  {
     "id": "28294ce0-0955-4550-806b-a00791abcdc2",
-    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    "created_at": "2022-10-10T00:00:00.000Z",
+    "title": "Implementar Swagger no server do projeto TO DO",
+    "created_at": "2022-07-07T00:00:00.000Z",
     "status": "to-do"
   },
   {
     "id": "10538f4a-722c-44a8-bbdf-bcd595e0c139",
-    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    "created_at": "2022-10-11T00:00:00.000Z",
+    "title": "Implementar rotas de API no projeto TO DO",
+    "created_at": "2022-06-06T00:00:00.000Z",
     "status": "done"
   },
   {
     "id": "5718ab6f-8566-474e-980b-5d536bd534d5",
-    "title": "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    "created_at": "2022-10-13T00:00:00.000Z",
+    "title": "Criar rotas no servidor do projeto TO DO",
+    "created_at": "2022-05-05T00:00:00.000Z",
     "status": "done"
+  },
+  {
+    "id": "bcb89cc4-6130-49d0-b39d-f1fd86db1779",
+    "title": "Implementar Storybook no projeto TO DO",
+    "created_at": "2022-09-09T00:00:00.000Z",
+    "status": "to-do"
+  },
+  {
+    "id": "764c7b98-c7c5-4362-a855-d79277bf71c2",
+    "title": "Implementar testes no projeto TO DO",
+    "created_at": "2022-08-08T00:00:00.000Z",
+    "status": "to-do"
   }
 ]
